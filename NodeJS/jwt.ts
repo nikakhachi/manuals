1dab0da42e44ada983c134a631c68ffced859f65
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2022-08-09 00:23:35 +0400

    move single files in folders out of the folders in NodeJS

--- NodeJS/jwt.ts
+++ NodeJS/jwt.ts
@@ -1,11 +1,13 @@
 import jwt from "jsonwebtoken";
 
-const signAccessToken = (id: string) => {
-  const accessToken = jwt.sign({ id }, process.env.JWT_KEY, { expiresIn: "2h" });
+export const signAccessToken = (id: string) => {
+  const accessToken = jwt.sign({ id }, process.env.JWT_KEY, {
+    expiresIn: "2h",
+  });
   return accessToken;
 };
 
-const verifyJwt = (accessToken: string) => {
+export const verifyJwt = (accessToken: string) => {
   try {
     const decoded = jwt.verify(accessToken, process.env.JWT_KEY);
   } catch (error) {
@@ -13,6 +15,6 @@ const verifyJwt = (accessToken: string) => {
   }
 };
 
-const decodeJwt = (cachedToken: string) => {
+export const decodeJwt = (cachedToken: string) => {
   const decodedJwt = jwt.decode(cachedToken, { complete: true });
 };
---
+++ NodeJS/logMemoryUsage.ts
@@ -0,0 +1,10 @@
+export const logMemoryUsage = () => {
+  const used: any = process.memoryUsage();
+  let text = "|";
+  for (let key in used) {
+    text += ` ${key} : ${
+      Math.round((used[key] / 1024 / 1024) * 100) / 100
+    } MB |`;
+  }
+  logger.info(text);
+};
