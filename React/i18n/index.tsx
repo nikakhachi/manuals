aad5a7fb2502a9999e04afec28557c678cc7fc17
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2022-08-09 00:02:42 +0400

    add i18n to React

---
+++ React/i18n/index.tsx
@@ -0,0 +1,24 @@
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import "./i18n";
+
+ReactDOM.render(
+  // <React.StrictMode>
+  <BrowserRouter>
+    <Suspense fallback="">
+      <App />
+    </Suspense>
+  </BrowserRouter>,
+  // </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register({});
